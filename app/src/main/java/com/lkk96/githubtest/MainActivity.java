ff73ad5122d11ef5cfc045b4e70e7eb8a272d8b6
alfo6-1 <rudrb43@naver.com>
2019-02-11 15:55:19 +0900

    aaa

---
+++ app/src/main/java/com/lkk96/githubtest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lkk96.githubtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Nice");
+    }
+}
